0cfc77b4195a3db55b301f78d19c92521c4c6948
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-12 08:27:10 +0000

    refactor(routes): Reestrutura roteamento organizando as rotas públicas e privadas (autenticadas)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth';
+import AppLayout from '@/layouts/AppLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/projetos',
-      name: 'listar-projetos',
-      component: () => import('../views/ListarProjetosView.vue'),
+      path: '/login',
+      component: () => import('../views/LoginView.vue'),
     },
     {
-      path: '/projetos/novo',
-      name: 'criar-projeto',
-      component: () => import('../views/CriarProjetoView.vue'),
+      path: '/',
+      component: AppLayout,
+      meta: {
+        requiresAuth: true,
+      },
+      children: [
+        {
+          path: '/projetos',
+          name: 'listar-projetos',
+          component: () => import('../views/ListarProjetosView.vue'),
+        },
+        {
+          path: '/projetos/novo',
+          name: 'criar-projeto',
+          component: () => import('../views/CriarProjetoView.vue'),
+        },
+        {
+          path: '/projetos/:id',
+          name: 'visualizar-projeto',
+          component: () => import('../views/ProjetoView.vue'),
+          props: true,
+        },
+        {
+          path: '/projetos/:id/editar',
+          name: 'editar-projeto',
+          component: () => import('../views/EditarProjetoView.vue'),
+          props: true,
+        }
+      ]
     },
-    {
-      path: '/projetos/:id',
-      name: 'visualizar-projeto',
-      component: () => import('../views/ProjetoView.vue'),
-      props: true,
-    }
   ],
+});
+
+router.beforeEach((to, _from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    return next('/login');
+  }
+
+  next();
 });
 
 export default router
