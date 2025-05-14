5661ac55ed1ccf4d903890521a94e3c91197a589
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-14 14:49:49 +0000

    refactor(connectors): Ajusta passagem da URL base a partir da __ENV__ na window

--- src/connectors/api.ts
+++ src/connectors/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiBaseUrl = (window as any).__ENV__?.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: apiBaseUrl,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
