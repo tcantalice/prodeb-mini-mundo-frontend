efe92e040444bca0cf52a898f1d0fbff411160f5
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-21 15:19:37 +0000

    refactor(layouts): Ajusta definição de tamanhos do layout (WIP)

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="w-screen">
     <header class="bg-indigo-600 text-white p-4 flex justify-between items-center">
       <h1 class="text-xl font-semibold">Painel do Sistema</h1>
       <button @click="logout" class="bg-indigo-800 px-3 py-1 rounded hover:bg-indigo-700">
@@ -7,7 +7,7 @@
       </button>
     </header>
 
-    <main class="flex-1 p-6 bg-gray-50">
+    <main class="flex-1 bg-gray-50">
       <router-view />
     </main>
   </div>
