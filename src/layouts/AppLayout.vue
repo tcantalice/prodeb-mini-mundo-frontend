b8c547351a830adb5497266af83ae45fe00b00c7
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-22 16:01:01 +0000

    refactor(layouts): Redefine importação do armazenador de estado de autenticação

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { useAuthStore } from '@/stores/auth';
+import { useAuthStore } from '@/ui/stores/auth';
 import { useRouter } from 'vue-router';
 
 const authStore = useAuthStore();
