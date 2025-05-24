5d19f27b997a9347f76ed06830a06eeaa6951b43
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-24 18:58:23 +0000

    refactor(views/projeto): Refatora detalhes do projeto para utilizar o presenter

--- src/ui/views/Projeto/components/DetalheProjeto.vue
+++ src/ui/views/Projeto/components/DetalheProjeto.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { ref } from 'vue';
+import { onMounted, ref, defineModel } from 'vue';
+import type GerenciamentoProjetoPresenter from '@/presenters/GerenciamentoProjetoPresenter';
+import type { ProjetoViewModel } from '@/presenters/interfaces/GerenciamentoProjetoView';
+
+const carregando = defineModel<boolean>('carregando', { required: true });
+
+const props = defineProps<{ presenter: GerenciamentoProjetoPresenter }>();
+const presenter = props.presenter;
 
 const projeto = ref<{
   id: string,
@@ -9,24 +16,40 @@ const projeto = ref<{
   descricao: string | null,
   orcamento: number | null,
   ativo: boolean
-}>({
-  id: '',
-  nome: 'Título',
-  descricao: null,
-  orcamento: null,
-  ativo: true
+}>();
+
+presenter.bindView({
+  bindProjeto: (projetoViewModel: ProjetoViewModel) => {
+    projeto.value = ({
+      id: projetoViewModel.id,
+      nome: projetoViewModel.nome,
+      ativo: projetoViewModel.ativo,
+      descricao: projetoViewModel.descricao,
+      orcamento: projetoViewModel.orcamento,
+    });
+  },
+  disableLoading: () => {
+    carregando.value = false;
+    console.log('Desabilitado loading')
+  },
+  enableLoading: () => carregando.value = true,
+  showValidationError: (field: string, message: string) => {}
+});
+
+onMounted(async () => {
+ presenter.onLoad();
 });
 </script>
 
 <template>
   <div class="p-2">
     <div class="flex align-middle gap-2">
-      <h1 class="text-4xl font-light">{{ projeto.nome }}</h1>
+      <h1 class="text-4xl font-light">{{ projeto?.nome }}</h1>
       <span class="p-1 rounded-full cursor-pointer">
         <FontAwesomeIcon :icon="faInfoCircle" />
       </span>
     </div>
-    <h2 class="text-neutral-400 italic">{{ projeto.ativo ? 'Ativo' : 'Inativo' }}</h2>
-    <p v-if="projeto.descricao" class="w-full text-sm mt-2">{{ projeto.descricao }}</p>
+    <h2 class="text-neutral-400 italic">{{ projeto?.ativo ? 'Ativo' : 'Inativo' }}</h2>
+    <p v-if="projeto?.descricao" class="w-full text-sm mt-2">{{ projeto.descricao }}</p>
   </div>
 </template>
