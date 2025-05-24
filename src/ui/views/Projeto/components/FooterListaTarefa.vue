d111a2ccb58a54df35dc0bf1ebda7d4b71969959
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-24 15:06:58 +0000

    refactor(views/projeto): Adiciona auto foco no campo de descrição

--- src/ui/views/Projeto/components/FooterListaTarefa.vue
+++ src/ui/views/Projeto/components/FooterListaTarefa.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 
 defineEmits(['onConfirmCreation']);
 
 const isCreating = ref<boolean>(false);
+
+const descricaoField = ref<HTMLInputElement>();
+const descricao = ref<string | null>(null);
+
+watch(isCreating, async (field) => {
+  if (field) {
+    await nextTick();
+    descricaoField.value?.focus();
+  }
+});
+
+const onLeaveDescricaoField = () => {
+  isCreating.value = false;
+  descricao.value = null;
+}
 </script>
 
 <template>
   <section>
     <div v-if="isCreating" class="p-2">
-      <input class="w-full" type="text" @blur="() => isCreating = false" @keypress.enter="$emit('onConfirmCreation')" :autofocus="true"/>
+      <input
+        ref="descricaoField"
+        v-model="descricao"
+        class="w-full focus:outline-0 focus:p-1"
+        type="text"
+        @blur="onLeaveDescricaoField"
+        @keypress.enter="$emit('onConfirmCreation', descricao)" />
     </div>
-    <div @click="() => isCreating = true" class="bg-gray-100 hover:bg-gray-200 cursor-pointer p-2 rounded-b-md border border-gray-200">
+    <div
+      @click="() => isCreating = true"
+      class="bg-gray-100 hover:bg-gray-200 cursor-pointer p-2 rounded-b-md border border-gray-200">
       <span class="font-bold">Adicionar +</span>
     </div>
   </section>
