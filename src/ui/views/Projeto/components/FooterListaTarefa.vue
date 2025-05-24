482b533eea4d7441e1aa50df0a737b92f71e38f2
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-24 02:59:10 +0000

    feat(views/projeto): Cria footer da listagem de tarefas do projeto (WIP)

---
+++ src/ui/views/Projeto/components/FooterListaTarefa.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineEmits(['onConfirmCreation']);
+
+const isCreating = ref<boolean>(false);
+</script>
+
+<template>
+  <section>
+    <div v-if="isCreating" class="p-2">
+      <input class="w-full" type="text" @blur="() => isCreating = false" @keypress.enter="$emit('onConfirmCreation')" :autofocus="true"/>
+    </div>
+    <div @click="() => isCreating = true" class="bg-gray-100 hover:bg-gray-200 cursor-pointer p-2 rounded-b-md border border-gray-200">
+      <span class="font-bold">Adicionar +</span>
+    </div>
+  </section>
+</template>
