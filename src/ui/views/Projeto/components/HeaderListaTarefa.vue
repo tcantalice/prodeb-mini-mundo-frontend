e280793a740cd0f76c7e53ca1fac182ded55a52b
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-24 02:26:29 +0000

    feat(views/projeto) Cria componente do header da listagem de tarefas

---
+++ src/ui/views/Projeto/components/HeaderListaTarefa.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex bg-gray-100">
+    <div class="flex-0 p-2 w-fit"><input type="checkbox" class="w-5" /></div>
+    <div class="text-center p-2 flex-3"><span>Tarefa</span></div>
+    <div class="text-center p-2 flex-1"><span>Status</span></div>
+    <div class="text-center p-2 flex-1"><span>Iniciado Em</span></div>
+    <div class="text-center p-2 flex-1"><span>Finalizado Em</span></div>
+  </div>
+</template>
